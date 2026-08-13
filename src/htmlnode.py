21c41e87c62a94e0e29cc75b6800df670421debb
swills123 <scottalanwills@gmail.com>
2026-08-13 10:39:51 -0700

    Add ParentNode class and text_node_to_html_node conversion function

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -34,5 +34,21 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag:str, children:list["HTMLNode"], props:dict[str,str] | None = None):
+        super().__init__(tag,None,children,props)
+        
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if self.children is None:
+            raise ValueError("ParentNode must have children")
+        
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+        
         
         
